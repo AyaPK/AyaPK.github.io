34ba2954a6e989a5c552594d5c6a9b2088b17a0c
AyaPK <Sherbnya@gmail.com>
2019-03-10 23:31:10 +0000

    Update cardGenerate.js

--- bingo/cardGenerate.js
+++ bingo/cardGenerate.js
@@ -24,14 +24,14 @@ function generateCard() {
    document.getElementById("lastNumber").innerHTML = (lastNumber);
    document.getElementById("bingoTest").innerHTML = ("");
 
-   function draw(){
-   for (a = 1; a < 10; a++) {
-      
-      chosenNumber = Math.floor(Math.random() * 99)
-      if(!cardNumbers.includes(chosenNumber)){
-      cardNumbers.push(chosenNumber)
-      }else{draw()}
-   }
+   function draw() {
+      for (a = 1; a < 10; a++) {
+
+         chosenNumber = Math.floor(Math.random() * 99)
+         if (!cardNumbers.includes(chosenNumber)) {
+            cardNumbers.push(chosenNumber)
+         } else { draw() }
+      }
    }
    draw()
    console.log(cardNumbers)
@@ -51,15 +51,15 @@ function generateCard() {
 
 //pull a number
 function test() {
-   numberDraw = Math.floor(Math.random()*99)
-   if(!pulledNumbers.includes(numberDraw)){
+   numberDraw = Math.floor(Math.random() * 99)
+   if (!pulledNumbers.includes(numberDraw)) {
       pulledNumbers.push(numberDraw)
       lastNumber = numberDraw
-      pulledNumbers.sort(function(a, b){return a-b})
-   document.getElementById("lastNumber").innerHTML = ("And the number is... <b>"+lastNumber+"!!!</b>");
-   document.getElementById("pulledNumbers").innerHTML = (pulledNumbers);
-   
-   }else{
+      pulledNumbers.sort(function (a, b) { return a - b })
+      document.getElementById("lastNumber").innerHTML = ("And the number is... <b>" + lastNumber + "!!!</b>");
+      document.getElementById("pulledNumbers").innerHTML = (pulledNumbers);
+
+   } else {
       test()
    }
 }
@@ -68,9 +68,9 @@ function test() {
 
 //check for matches
 function check() {
-   for (b=0; b<pulledNumbers.length; b++){
-      if(pulledNumbers.includes(cardNumbers[b])){
-         cardNumbers[b] = ("<p style=\"background-color:lightgreen\"><s>"+cardNumbers[b]+"</s></p>")
+   for (b = 0; b < pulledNumbers.length; b++) {
+      if (pulledNumbers.includes(cardNumbers[b])) {
+         cardNumbers[b] = ("<p style=\"background-color:lightgreen\"><s>" + cardNumbers[b] + "</s></p>")
          document.getElementById("number1").innerHTML = (cardNumbers[0]);
          document.getElementById("number2").innerHTML = (cardNumbers[1]);
          document.getElementById("number3").innerHTML = (cardNumbers[2]);
@@ -88,7 +88,7 @@ function check() {
 
 //reset game
 function reset() {
-   cardNumbers = ["","","","","","","","",""]
+   cardNumbers = ["", "", "", "", "", "", "", "", ""]
    pulledNumbers = [""]
    lastNumber = [""]
    bingo = false
@@ -108,45 +108,45 @@ function reset() {
 //end of reset
 
 //check for bingo
-function bingoCheck(){
-num1 = cardNumbers[0]
+function bingoCheck() {
+   num1 = cardNumbers[0]
 
-   if(/<s>/.test(cardNumbers[0])){match1 = true}else{match1 = false}  
-   if(/<s>/.test(cardNumbers[1])){match2 = true}else{match2 = false} 
-   if(/<s>/.test(cardNumbers[2])){match3 = true}else{match3 = false} 
-   if(/<s>/.test(cardNumbers[3])){match4 = true}else{match4 = false} 
-   if(/<s>/.test(cardNumbers[4])){match5 = true}else{match5 = false} 
-   if(/<s>/.test(cardNumbers[5])){match6 = true}else{match6 = false} 
-   if(/<s>/.test(cardNumbers[6])){match7 = true}else{match7 = false} 
-   if(/<s>/.test(cardNumbers[7])){match8 = true}else{match8 = false} 
-   if(/<s>/.test(cardNumbers[8])){match9 = true}else{match9 = false} 
+   if (/<s>/.test(cardNumbers[0])) { match1 = true } else { match1 = false }
+   if (/<s>/.test(cardNumbers[1])) { match2 = true } else { match2 = false }
+   if (/<s>/.test(cardNumbers[2])) { match3 = true } else { match3 = false }
+   if (/<s>/.test(cardNumbers[3])) { match4 = true } else { match4 = false }
+   if (/<s>/.test(cardNumbers[4])) { match5 = true } else { match5 = false }
+   if (/<s>/.test(cardNumbers[5])) { match6 = true } else { match6 = false }
+   if (/<s>/.test(cardNumbers[6])) { match7 = true } else { match7 = false }
+   if (/<s>/.test(cardNumbers[7])) { match8 = true } else { match8 = false }
+   if (/<s>/.test(cardNumbers[8])) { match9 = true } else { match9 = false }
 
-   if(match1 && match2 && match3){
+   if (match1 && match2 && match3) {
       bingo = true
-      }else if(match4 && match5 && match6){
-      bingo=true
-      }else if(match7 && match8 && match9){
-         bingo=true
-         }else if(match1 && match4 && match7){
-            bingo=true
-            }else if(match2 && match5 && match8){
-               bingo=true
-               }else if(match3 && match6 && match9){
-                  bingo=true
-                  }else if(match1 && match5 && match9){
-                     bingo=true
-                     }else if(match3 && match5 && match7){
-                        bingo=true
-                        }else{
-                           bingo=false
-                        }
+   } else if (match4 && match5 && match6) {
+      bingo = true
+   } else if (match7 && match8 && match9) {
+      bingo = true
+   } else if (match1 && match4 && match7) {
+      bingo = true
+   } else if (match2 && match5 && match8) {
+      bingo = true
+   } else if (match3 && match6 && match9) {
+      bingo = true
+   } else if (match1 && match5 && match9) {
+      bingo = true
+   } else if (match3 && match5 && match7) {
+      bingo = true
+   } else {
+      bingo = false
+   }
 
 
-if(bingo){
-   document.getElementById("bingoTest").innerHTML = ("<b>BINGO!!!</b>");
-}else{
-   document.getElementById("bingoTest").innerHTML = ("No bingo found...?");
-}
+   if (bingo) {
+      document.getElementById("bingoTest").innerHTML = ("<b>BINGO!!!</b>");
+   } else {
+      document.getElementById("bingoTest").innerHTML = ("No bingo found...?");
+   }
 
 }
 //
