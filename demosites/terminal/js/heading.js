21aa24c92384f182d0e5f80a1cc2705c164cbd81
Aya <47668950+AyaPK@users.noreply.github.com>
2021-04-19 18:41:48 +0100

    Terminal submenus

--- demosites/terminal/js/heading.js
+++ demosites/terminal/js/heading.js
@@ -1,15 +1,18 @@
-var stringToAdd = $(".animateTextArea").html();
-$(".animateTextArea").html("> ");
-var index = 0;
-function addLetter(){
-    $(".animateTextArea").append(stringToAdd[index]);
-    index++;
-    if(index > stringToAdd.length){
-        clearInterval(running);
+
+function animation(){
+    var stringToAdd = $(".animateTextArea").html();
+    $(".animateTextArea").html("> ");
+    var index = 0;
+    function addLetter(){
+        $(".animateTextArea").append(stringToAdd[index]);
+        index++;
     }
+
+    jQuery(document).ready(function() {
+        setTimeout(function() {
+            var running = setInterval(addLetter, 100);
+        }, 1000);
+    });
 }
-jQuery(document).ready(function() {
-    setTimeout(function() {
-        var running = setInterval(addLetter, 100);
-    }, 1000);
-});
+
+animation();
